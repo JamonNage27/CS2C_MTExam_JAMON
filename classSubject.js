694fa9d2753461d4c2a61b46eda0f4fd709a9db4
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-10-31 11:33:22 +0800

    classSubject.js

--- classSubject.js
+++ classSubject.js
@@ -5,4 +5,4 @@ let classroom = "ComLabC";
 let classInstructor = "Mr. Batin";
 let studentName = "Nagely Jamon";
 
-console.log(
+console.log('${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}');
