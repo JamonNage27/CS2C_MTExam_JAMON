e1212691b2c650c0389820b01fd309842af7f8c4
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-10-31 10:14:18 +0800

    classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,8 @@
+//classSubject.js
+let subjectTitle = "Discrete Structure";
+let classSched = "1:30 PM - 3:00 PM, Tuesday and Thursday";
+let classroom = "ComLabC";
+let classInstructor = "Mr. Batin";
+let studentName = "Nagely Jamon";
+
+console.log(
