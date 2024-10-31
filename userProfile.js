47c1433efc6257d4521e25531771c02fc19b81de
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-10-31 10:04:26 +0800

    userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,10 @@
+//userProfile.js
+let fName = "Nagely Jamon";
+let age = 19;
+let favNum = 27;
+let favColor = "Purple";
+
+console.log("Full Name:" + fName);
+console.log("Age:" + age);
+console.log("Favorite Number:" + favNum);
+console.log("Favorite Color:" + favColor);
